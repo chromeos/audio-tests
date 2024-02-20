2460c2cbe115c9e8bd4b7a82926ed504fe837504
Li-Yu Yu <aaronyu@google.com>
2024-02-20 14:17:03 +0000

    ds-playground: Initial version

--- src/ds-playground/src/ds-playground.ts
+++ src/ds-playground/src/ds-playground.ts
@@ -1,81 +1,377 @@
-import { LitElement, html, css } from 'lit';
-import { property, customElement } from 'lit/decorators.js';
+import { LitElement, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+import { join } from 'lit/directives/join.js';
+import '@material/web/chips/chip-set';
+import '@material/web/chips/suggestion-chip';
+import '@material/web/chips/filter-chip';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+enum DeviceType {
+  Internal = 'Internal',
+  ThreePointFive = '3.5mm',
+  USB = 'USB',
+  HDMI = 'HDMI',
+  Bluetooth = 'Bluetooth',
+}
 
+type Device = {
+  ty: DeviceType;
+  name: string;
+};
+
+function defaultDevices() {
+  const spec: [DeviceType, number][] = [
+    [DeviceType.Internal, 1],
+    [DeviceType.ThreePointFive, 1],
+    [DeviceType.USB, 3],
+    [DeviceType.HDMI, 3],
+    [DeviceType.Bluetooth, 3],
+  ];
+  const devices: Device[] = [];
+  for (const [ty, count] of spec) {
+    if (count === 1) {
+      devices.push({ ty: ty, name: ty });
+    } else {
+      for (let i = 1; i <= count; i++) {
+        devices.push({ ty: ty, name: `${ty} ${i}` });
+      }
+    }
+  }
+  return devices;
+}
+
+abstract class Strategy {
+  abstract probe(devices: Device[]): Device | null;
+  abstract select(device: Device): void;
+  abstract clone(): Strategy;
+  visualize(): unknown {
+    return 'N/A';
+  }
+}
+
+function contains(devices: Device[], device: Device) {
+  return devices.some(x => device.name == x.name);
+}
+
+class PriorityListStrategy extends Strategy {
+  connectedDevices: Device[] = [];
+  priorityList: Device[] = [];
+  active: Device | null = null;
+
+  override clone(): PriorityListStrategy {
+    const s = new PriorityListStrategy();
+    s.connectedDevices = [...this.connectedDevices];
+    s.priorityList = [...this.priorityList];
+    s.active = this.active;
+    return s;
+  }
+
+  override probe(newDevices: Device[]): Device | null {
+    this.connectedDevices = this.connectedDevices.filter(x =>
+      contains(newDevices, x)
+    );
+    const plugged = newDevices.filter(x => !contains(this.connectedDevices, x));
+
+    // The active device is unplugged.
+    if (this.active && !contains(this.connectedDevices, this.active)) {
+      this.active = null;
+    }
+
+    if (this.active === null) {
+      this.active = this.connectedDevices.reduce(
+        (accumulator: Device | null, currentValue: Device) => {
+          const currentPriority = this.priorityList.findIndex(
+            x => x.name == currentValue.name
+          );
+          if (currentPriority === -1) {
+            return accumulator;
+          }
+          if (accumulator === null) {
+            return currentValue;
+          }
+          const accumulatorPriority = this.priorityList.findIndex(
+            x => x.name === accumulator.name
+          );
+          if (accumulatorPriority < currentPriority) {
+            return currentValue;
+          }
+          return accumulator;
+        },
+        null
+      );
+    }
+
+    plugged.sort(
+      (a, b) =>
+        (a.ty === DeviceType.ThreePointFive ? 1 : 0) -
+        (b.ty === DeviceType.ThreePointFive ? 1 : 0)
+    );
+
+    for (const hotplug of plugged) {
+      this.connectedDevices.push(hotplug);
+      if (this.shouldSwitchToHotPlugDevice(this.active, hotplug)) {
+        this.select(hotplug);
+      }
+    }
+
+    return this.active;
+  }
+
+  override select(device: Device): void {
+    if (!contains(this.connectedDevices, device)) {
+      throw new Error(
+        `${
+          device.name
+        } is not connected. Connected: ${this.connectedDevices.map(
+          x => x.name
+        )}`
+      );
+    }
+    this.active = device;
+    this.bubbleUp(device);
+  }
+
+  override visualize() {
+    return html`User Priority:
+    ${this.priorityList.length > 0
+      ? join(
+          this.priorityList.map(d => html`${d.name}`),
+          ' < '
+        )
+      : '(empty)'}`;
+  }
+
+  bubbleUp(device: Device) {
+    if (!contains(this.priorityList, device)) {
+      this.priorityList.unshift(device);
+    }
+    const connectedDeviceNames = new Set(
+      this.connectedDevices.map(x => x.name)
+    );
+    const fromIndex = this.priorityList.findIndex(x => x.name === device.name);
+    let toIndex = 0;
+    for (const [i, device] of this.priorityList.entries()) {
+      if (connectedDeviceNames.has(device.name)) {
+        toIndex = i;
+      }
+    }
+    this.priorityList.splice(fromIndex, 1);
+    this.priorityList.splice(toIndex, 0, device);
+  }
+
+  shouldSwitchToHotPlugDevice(
+    current: Device | null,
+    hotplug: Device
+  ): boolean {
+    if (current === null) {
+      return true;
+    }
+    if (hotplug.ty === DeviceType.ThreePointFive) {
+      return true;
+    }
+
+    const currentUserPriority = this.getUserPriority(current);
+    const hotplugUserPriority = this.getUserPriority(hotplug);
+    if (currentUserPriority !== null && hotplugUserPriority !== null) {
+      return currentUserPriority <= hotplugUserPriority;
+    }
+    return this.getBuiltinPriority(current) <= this.getBuiltinPriority(hotplug);
+  }
+
+  getUserPriority(device: Device): number | null {
+    const index = this.priorityList.findIndex(x => x.name == device.name);
+    if (index === -1) {
+      return null;
+    }
+    return index;
+  }
+
+  // See https://source.chromium.org/chromium/chromium/src/+/main:chromeos/ash/components/audio/audio_device.cc;l=22;drc=327564a4861822c816d35395dfb54d7e5039e6ea.
+  getBuiltinPriority(device: Device): number {
+    switch (device.ty) {
+      case DeviceType.ThreePointFive:
+      case DeviceType.USB:
+      case DeviceType.Bluetooth:
+        return 3;
+      case DeviceType.HDMI:
+        return 2;
+      case DeviceType.Internal:
+        return 1;
+    }
+  }
+}
+
+class State {
+  name: string;
+  connectedDevices: Device[] = [];
+  active: Device | null = null;
+  strategy: Strategy;
+  constructor(name: string, strategy: Strategy) {
+    this.name = name;
+    this.strategy = strategy;
+  }
+
+  private clone(name: string) {
+    const s = new State(name, this.strategy.clone());
+    s.connectedDevices = [...this.connectedDevices];
+    s.active = this.active;
+    return s;
+  }
+
+  select(device: Device): State {
+    const s = this.clone(`Select ${device.name}`);
+    s.strategy.select(device);
+    s.active = device;
+    return s;
+  }
+
+  plug(device: Device): State {
+    const s = this.clone(`Plug ${device.name}`);
+    s.connectedDevices.push(device);
+    s.active = s.strategy.probe(s.connectedDevices);
+    return s;
+  }
+
+  unplug(device: Device): State {
+    const s = this.clone(`Unplug ${device.name}`);
+    s.connectedDevices = s.connectedDevices.filter(x => x.name !== device.name);
+    s.active = s.strategy.probe(s.connectedDevices);
+    return s;
+  }
+}
+
+@customElement('ds-legend')
+export class DsLegend extends LitElement {
+  protected override render() {
+    return html` <section>
+      <h3>Help</h3>
+      <ul>
+        <li>
+          <p>
+            <md-chip-set
+              ><md-filter-chip
+                label="Device"
+                selected
+                elevated
+                removable
+                @click=${(e: Event) => e.preventDefault()}
+                @remove=${(e: Event) => e.preventDefault()}
+              ></md-filter-chip>
+              This is a connected, active device. Click on "X" to
+              unplug.</md-chip-set
+            >
+          </p>
+        </li>
+        <li>
+          <p>
+            <md-chip-set
+              ><md-filter-chip
+                label="Device"
+                elevated
+                removable
+                @click=${(e: Event) => e.preventDefault()}
+                @remove=${(e: Event) => e.preventDefault()}
+              ></md-filter-chip>
+              This is a connected device. Click on it to select. Click on "X" to
+              unplug.</md-chip-set
+            >
+          </p>
+        </li>
+        <li>
+          <p>
+            <md-chip-set
+              ><md-suggestion-chip label="Device"></md-suggestion-chip> This is
+              a disconnected device. Click on it to plug.</md-chip-set
+            >
+          </p>
+        </li>
+      </ul>
+    </section>`;
+  }
+}
+
+@customElement('ds-state')
+export class DsState extends LitElement {
+  @state()
+  state: State | null = null;
+  pushFunc = (_s: State) => {};
+
+  protected override render() {
+    return html`
+      <section>
+        <h4>${this.s.name}</h4>
+        <p>
+          <md-chip-set>
+            ${defaultDevices().map(x => this.renderDevice(x))}
+          </md-chip-set>
+        </p>
+        <p>${this.s.strategy.visualize()}</p>
+      </section>
+    `;
+  }
+
+  get s(): State {
+    return this.state!;
+  }
+
+  renderDevice(device: Device) {
+    if (contains(this.s.connectedDevices, device)) {
+      return html`<md-filter-chip
+        label=${device.name}
+        elevated
+        removable
+        ?selected=${this.s.active?.name === device.name}
+        @remove=${(e: Event) => {
+          e.preventDefault();
+          this.pushFunc(this.s.unplug(device));
+        }}
+        @click=${(e: Event) => {
+          e.preventDefault();
+          if (this.s.active?.name === device.name) {
+            return;
+          }
+          this.pushFunc(this.s.select(device));
+        }}
+      ></md-filter-chip>`;
+    }
+    return html`<md-suggestion-chip
+      label=${device.name}
+      @click=${() => this.pushFunc(this.s.plug(device))}
+    ></md-suggestion-chip>`;
+  }
+}
+
+/**
+ * An example element.
+ *
+ * @fires count-changed - Indicates when the count changes
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
 @customElement('ds-playground')
 export class DsPlayground extends LitElement {
-  @property({ type: String }) header = 'My app';
+  @state()
+  steps: State[] = [new State('Initial state', new PriorityListStrategy())];
 
-  static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--ds-playground-background-color);
-    }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
-  `;
-
-  render() {
+  override render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/DsPlayground.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+      <ds-legend></ds-legend>
+      <h3>Playground</h3>
+      <ol>
+        ${[...this.steps.entries()].map(
+          ([i, s]) =>
+            html`<li>
+              <ds-state
+                .state=${s}
+                .pushFunc=${(s: State) => this.push(i, s)}
+              ></ds-state>
+            </li>`
+        )}
+      </ol>
     `;
+  }
+
+  push(index: number, s: State) {
+    this.steps.splice(index + 1, this.steps.length - index - 1, s);
+    this.requestUpdate();
   }
 }
